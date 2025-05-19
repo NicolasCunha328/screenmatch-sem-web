48b05c0eb1bee300aed19e56157ee794234fb669
Nicolas <nico124cunha@gmail.com>
2025-05-19 14:10:23 -0300

    Modelando categorias e séries

--- src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -3,8 +3,14 @@ package br.com.alura.screenmatch.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.awt.*;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosSerie(@JsonAlias("Title") String titulo,
                          @JsonAlias("totalSeasons") Integer totalTemporadas,
-                         @JsonAlias("imdbRating") String avaliacao) {
+                         @JsonAlias("imdbRating") String avaliacao,
+                         @JsonAlias("Actors") String atores,
+                         @JsonAlias("Genre") String genero,
+                         @JsonAlias("Plot") String sinopse,
+                         @JsonAlias("Poster") String poster) {
 }
