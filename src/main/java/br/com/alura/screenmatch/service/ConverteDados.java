f1b0e883d01513f498ddc8e7be4d17d333cf1155
Nicolas <nico124cunha@gmail.com>
2025-08-18 10:33:15 -0300

    Buscando filmes por ano de lançamento e buscando top 5 melhores episódios por série

--- src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
+++ src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ConverteDados implements IConverteDados { // implementa a interface IConverteDados e é responsável por converter dados JSON em objetos Java usando a biblioteca Jackson (ObjectMapper)
 
-    private ObjectMapper mapper = new ObjectMapper(); // objeto usado para desserialização do JSON
+    private final ObjectMapper mapper = new ObjectMapper(); // objeto usado para desserialização do JSON
 
     @Override
     public <T> T obterDados(String json, Class<T> classe) { // metodo da interface IConverteDados
